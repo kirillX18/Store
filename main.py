c059262652ce58944b2a15912acc4d81a514017d
KirillKoksharov <91093503+kirillX18@users.noreply.github.com>
2024-02-07 19:25:15 +0500

    Коррекция ошибок в сообщениях

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main(message):
     con.close()
 
 
-    bot.send_message(message.chat.id, 'Привет это магазин товаров давайте зарегестрируем вас, напишите своё имя')
+    bot.send_message(message.chat.id, 'Привет, это магазин товаров давайте зарегистрируем вас, напишите своё имя')
     bot.register_next_step_handler(message, user_name)
 
 def user_name(message):
@@ -28,7 +28,7 @@ def user_name(message):
 def user_amel(message):
     global amel
     amel = message.text.strip()
-    bot.send_message(message.chat.id, 'Ведите пороль')
+    bot.send_message(message.chat.id, 'Ведите пароль')
     bot.register_next_step_handler(message, user_pass)
 
 def user_pass(message):
@@ -49,29 +49,29 @@ def user_pass(message):
     markup.row(btn1, btn2)
     markup.row(btn3, btn4)
 
-    bot.send_message(message.chat.id, f'Здрвсвуйте {name} вы успешно зарегестрировались', reply_markup=markup)
+    bot.send_message(message.chat.id, f'Здравствуйте {name}, вы успешно зарегистрировались', reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda callback: True)
 def callback_massage(callback):
     markup1 = types.InlineKeyboardMarkup(row_width=1)
-    markup1.add(types.InlineKeyboardButton('ЧикенБургер', callback_data='KL'),
+    markup1.add(types.InlineKeyboardButton('Чекинбургер', callback_data='KL'),
                 types.InlineKeyboardButton('Фишбургер', callback_data='F'),
                 types.InlineKeyboardButton('Чисбургер', callback_data='C'))
 
     markup2 = types.InlineKeyboardMarkup(row_width=1)
-    markup2.add(types.InlineKeyboardButton('Спйси', callback_data='KL'),
-                types.InlineKeyboardButton('Класика', callback_data='F'),
+    markup2.add(types.InlineKeyboardButton('Спайси', callback_data='KL'),
+                types.InlineKeyboardButton('Классика', callback_data='F'),
                 types.InlineKeyboardButton('Фитнес', callback_data='C'))
 
     markup3 = types.InlineKeyboardMarkup(row_width=1)
-    markup3.add(types.InlineKeyboardButton('Грутка', callback_data='KL'),
+    markup3.add(types.InlineKeyboardButton('Грудка', callback_data='KL'),
                 types.InlineKeyboardButton('Ножка', callback_data='F'),
-                types.InlineKeyboardButton('Крыля', callback_data='C'))
+                types.InlineKeyboardButton('Крылья', callback_data='C'))
 
     markup4 = types.InlineKeyboardMarkup(row_width=1)
-    markup4.add(types.InlineKeyboardButton('ЧикенБургер', callback_data='KL'),
-                types.InlineKeyboardButton('Фишбургер', callback_data='F'),
+    markup4.add(types.InlineKeyboardButton('Кола', callback_data='KL'),
+                types.InlineKeyboardButton('Спрайт', callback_data='F'),
                 types.InlineKeyboardButton('Липтон', callback_data='C'))
 
     if callback.data == 'byrger':
